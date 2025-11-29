d9d931679a3beef9f5023fe1d37b1ab7a3388f59
Fabian Betancourt <fabianrbv@gmail.com>
2025-11-29 18:51:00 -0500

    2.1

--- models/User.js
+++ models/User.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  googleId: {
+  githubId: {
     type: String,
     required: true,
     unique: true
   },
   displayName: String,
+  username: String,
   email: String,
+  avatar: String
 });
 
 const User = mongoose.model("User", userSchema);
